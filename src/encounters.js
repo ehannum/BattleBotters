0ec0a95327c2a07255a975d1efc4c86dc62d36b0
ehannum <ehannum@gmail.com>
2014-07-25 13:26:01 -0700

    [WIP] basic loot system and enemy scaling

--- src/encounters.js
+++ src/encounters.js
@@ -1,28 +1,48 @@
 // enemies and other encounters
 
 var encounters = {
-  rat: {
+  Rat: {
+    symbol: 'R',
     name: 'Sick Rat',
-    level: 1,
-    health: 3,
-    dmg: 1
-  },
-  wolf: {
-    name: 'Feral Wolf',
     level: 1,
     health: 5,
     dmg: 1
   },
+  wolf: {
+    symbol: 'W',
+    name: 'Feral Wolf',
+    level: 1,
+    health: 10,
+    dmg: 1
+  },
   alpha: {
+    symbol: 'A',
     name: 'Alpha Wolf',
     level: 2,
-    health: 8,
+    health: 16,
     dmg: 2
   },
   ghost: {
+    symbol: 'G',
     name: 'Spooky Ghost',
     level: 2,
     health: 1,
     dmg: 10
+  },
+  lackey: {
+    symbol: 'L',
+    name: 'Eight Finger Triad Lackey',
+    level: 3,
+    health: 28,
+    dmg: 3,
+    loot: {name: 'Retractable Kneecapper', type: 'wep', dmg: 2}
+  },
+  lionTurtle: {
+    symbol: 'A',
+    name: 'White Tiger Huajaka',
+    level: 50,
+    health: 820,
+    dmg: 55,
+    loot: {name: 'Huajaka\'s Razor Claw' , type: 'wep', dmg: 35}
   }
 };
